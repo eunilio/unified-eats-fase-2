e9309d783a65cdf881a9c1e3f67ca8af3da6f956
Felipe Eunilio Vieira dos Santos <felipe.eunilio@gmail.com>
2026-02-01 02:52:13 -0300

    Criar base do projeto Spring Boot e estrutura inicial de pacotes

---
+++ src/main/java/br/com/fiap/unifiedeats2/Unifiedeats2Application.java
@@ -0,0 +1,13 @@
+package br.com.fiap.unifiedeats2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Unifiedeats2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Unifiedeats2Application.class, args);
+	}
+
+}
